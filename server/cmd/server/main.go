6270d34b7b44e7b5244955cfca9f9eb12bd357e0
Vano7577 <vano7577@gmail.com>
2020-11-30 19:21:36 +0200

    add wire

--- server/cmd/server/main.go
+++ server/cmd/server/main.go
@@ -1,12 +1,54 @@
 package main
 
-import "github.com/OlegVanyaGreatBand/architecture-lab-3/server/models"
+import (
+	"database/sql"
+	"flag"
+	"github.com/OlegVanyaGreatBand/architecture-lab-3/server/db"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+)
+
+var httpPortNumber = flag.Int("p", 8080, "HTTP port number")
+
+func NewDbConnection() (*sql.DB, error) {
+	conn := &db.Connection{
+		DbName:     "multimedia_classroom",
+		User:       "user",
+		Host:       "localhost",
+		DisableSSL: true,
+	}
+	return conn.Open()
+}
 
 func main() {
-	tablet := models.TelemetryData{
-		TabletId:   0,
-		TabletName: nil,
+	// Parse command line arguments. Port number may be defined with "-p" flag.
+	flag.Parse()
+
+	// Create the server.
+	if server, err := ComposeApiServer(HttpPortNumber(*httpPortNumber)); err == nil {
+		// Start it.
+		go func() {
+			log.Println("Starting chat server...")
+
+			err := server.Start()
+			if err == http.ErrServerClosed {
+				log.Printf("HTTP server stopped")
+			} else {
+				log.Fatalf("Cannot start HTTP server: %s", err)
+			}
+		}()
+
+		// Wait for Ctrl-C signal.
+		sigChannel := make(chan os.Signal, 1)
+		signal.Notify(sigChannel, os.Interrupt)
+		<-sigChannel
+
+		if err := server.Stop(); err != nil && err != http.ErrServerClosed {
+			log.Printf("Error stopping the server: %s", err)
+		}
+	} else {
+		log.Fatalf("Cannot initialize chat server: %s", err)
 	}
-	println(tablet.TabletId)
-	println(tablet.TabletName)
 }
---
+++ server/cmd/server/server.go
@@ -0,0 +1,50 @@
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/OlegVanyaGreatBand/architecture-lab-3/server/models"
+	"net/http"
+)
+
+type HttpPortNumber int
+
+// TelemetryApiServer configures necessary handlers and starts listening on a configured port.
+type TelemetryApiServer struct {
+	Port HttpPortNumber
+
+	TelemetryHandler models.TelemetryHttpHandler
+
+	server *http.Server
+}
+
+// Start will set all handlers and start listening.
+// If this methods succeeds, it does not return until server is shut down.
+// Returned error will never be nil.
+func (s *TelemetryApiServer) Start() error {
+	if s.TelemetryHandler == nil {
+		return fmt.Errorf("channels HTTP handler is not defined - cannot start")
+	}
+	if s.Port == 0 {
+		return fmt.Errorf("port is not defined")
+	}
+
+	handler := new(http.ServeMux)
+	handler.HandleFunc("/channels", s.TelemetryHandler)
+
+	s.server = &http.Server{
+		Addr:    fmt.Sprintf(":%d", s.Port),
+		Handler: handler,
+	}
+
+	return s.server.ListenAndServe()
+}
+
+// Stops will shut down previously started HTTP server.
+func (s *TelemetryApiServer) Stop() error {
+	if s.server == nil {
+		return fmt.Errorf("server was not started")
+	}
+	return s.server.Shutdown(context.Background())
+}
